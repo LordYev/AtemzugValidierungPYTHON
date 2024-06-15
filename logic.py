9e39684a86abb5a95d8651d7458e475d8e665d2d
Yevgeniy Palamarchuk <yevgeniy.palamarchuk@web.de>
2024-06-15 16:23:54 +0200

    x-Achsen Label im Plot angepasst - Zeigt jetzt die aktuelle Intervalldauer an - Zeichen ersetzt -> '' zu ""

--- logic.py
+++ logic.py
@@ -58,8 +58,8 @@ class AtemzugValidierungLogic:
         # erstellt eine Matplotlib-Figur mit einer Zeile & Spalte (Breite x Höhe)
         fig, ax = plt.subplots(1, 1, figsize=(9, 4))
         ax.set_ylim(self.y_min, self.y_max)
-        ax.set_ylabel('Druck in mbar')
-        ax.set_xlabel('Zeit in Sekunden')
+        ax.set_ylabel("Druck in mbar")
+        ax.set_xlabel("Zeit in Sekunden")
         ax.grid(True)
 
         # Setze die Schritte für die y-Achse auf 2,5
@@ -72,7 +72,7 @@ class AtemzugValidierungLogic:
         start_sync_point = None
         end_sync_point = None
 
-        start_index = int(0 * self.mask_edf_meta_data['sfreq'])
+        start_index = int(0 * self.mask_edf_meta_data["sfreq"])
 
         # For-Schleife läuft vom start_index solange durch, bis es den Anfang des 1ten Synchronisierungspunktes ermittelt hat
         for i in range(start_index, len(self.mask_edf_data[0]) - 1):
@@ -80,7 +80,7 @@ class AtemzugValidierungLogic:
                 start_sync_point = i / 10000
                 break
 
-        start_index = int((self.duration_mask - 1) * self.mask_edf_meta_data['sfreq'])
+        start_index = int((self.duration_mask - 1) * self.mask_edf_meta_data["sfreq"])
 
         # For-Schleife läuft rückwärts vom start_index solange durch, bis es das Ende des 2ten Synchronisierungspunktes ermittelt hat
         for i in reversed(range(start_index)):
@@ -89,12 +89,12 @@ class AtemzugValidierungLogic:
                 break
 
         # Festlegen der Punkte zwischen welchen die Atemzüge ermittelt werden sollen
-        self.breath_search_start_point = int((start_sync_point + 0.6) * self.mask_edf_meta_data['sfreq'])
-        self.breath_search_end_point = int((end_sync_point - 0.6) * self.mask_edf_meta_data['sfreq'])
+        self.breath_search_start_point = int((start_sync_point + 0.6) * self.mask_edf_meta_data["sfreq"])
+        self.breath_search_end_point = int((end_sync_point - 0.6) * self.mask_edf_meta_data["sfreq"])
 
         # Startpunkt wird um 5sec und Endpunkt um 30sec nach links verschoben
-        start_sync_point = int((start_sync_point - 0.05) * self.mask_edf_meta_data['sfreq'])
-        end_sync_point = int((end_sync_point - 0.3) * self.mask_edf_meta_data['sfreq'])
+        start_sync_point = int((start_sync_point - 0.05) * self.mask_edf_meta_data["sfreq"])
+        end_sync_point = int((end_sync_point - 0.3) * self.mask_edf_meta_data["sfreq"])
 
         return start_sync_point, end_sync_point
 
@@ -109,8 +109,8 @@ class AtemzugValidierungLogic:
 
         for i in range(len(results)):
             # Bestimme Start- und Endpunkt des 1ten Synchronisierungspunktes
-            start_index = int(sync_point * self.mask_edf_meta_data['sfreq'])
-            end_index = int((sync_point + 60) * self.mask_edf_meta_data['sfreq'])
+            start_index = int(sync_point * self.mask_edf_meta_data["sfreq"])
+            end_index = int((sync_point + 60) * self.mask_edf_meta_data["sfreq"])
 
             # Begrenze beide Kurven auf das Intervall zwischen start_index und end_index
             mask_timeframe = self.mask_edf_data[:, start_index:end_index]
@@ -121,8 +121,8 @@ class AtemzugValidierungLogic:
             device_max_index = np.argmax(device_timeframe, axis=1)
 
             # Finde die Zeitpunkte der maximalen Werte
-            mask_sync_point_times = (start_index + mask_max_index) / self.mask_edf_meta_data['sfreq']
-            device_sync_point_times = (start_index + device_max_index) / self.device_edf_meta_data['sfreq']
+            mask_sync_point_times = (start_index + mask_max_index) / self.mask_edf_meta_data["sfreq"]
+            device_sync_point_times = (start_index + device_max_index) / self.device_edf_meta_data["sfreq"]
 
             # Errechnet die Zeitdifferenz zwischen den beiden Maximalwerten der Kurven
             results[i] = mask_sync_point_times - device_sync_point_times
@@ -147,18 +147,18 @@ class AtemzugValidierungLogic:
             end_point = starting_point + interval
 
             # Start- und Endindex für Mask- und Device-Kurve berechnen (Zeitpunkt * Abtastrate)
-            mask_start_index = int(starting_point * mask_edf_meta_data['sfreq'])
-            mask_end_index = int(end_point * mask_edf_meta_data['sfreq'])
+            mask_start_index = int(starting_point * mask_edf_meta_data["sfreq"])
+            mask_end_index = int(end_point * mask_edf_meta_data["sfreq"])
 
-            device_start_index = int((starting_point - self.time_difference_start[0]) * device_edf_meta_data['sfreq'])
-            device_end_index = int((end_point - self.time_difference_end[0]) * device_edf_meta_data['sfreq'])
+            device_start_index = int((starting_point - self.time_difference_start[0]) * device_edf_meta_data["sfreq"])
+            device_end_index = int((end_point - self.time_difference_end[0]) * device_edf_meta_data["sfreq"])
 
             # Skalierung der Device-Kurve
             scaled_device_data = self.scale_factor * device_edf_data[0, device_start_index:device_end_index]
 
             # Zeitintervall für Mask und Device erzeugen
-            mask_interval = np.arange(starting_point, end_point, 1 / mask_edf_meta_data['sfreq'])
-            device_interval = np.arange(starting_point, end_point, 1 / mask_edf_meta_data['sfreq'])
+            mask_interval = np.arange(starting_point, end_point, 1 / mask_edf_meta_data["sfreq"])
+            device_interval = np.arange(starting_point, end_point, 1 / mask_edf_meta_data["sfreq"])
 
             # Bestimme das kürzere Array aus Mask oder Device
             min_length = min(len(mask_interval), len(device_interval))
@@ -176,11 +176,11 @@ class AtemzugValidierungLogic:
                 line.remove()
 
             # Plottet die neuen Linien für das Intervall
-            ax.plot(mask_interval, mask_edf_data[0, mask_start_index:mask_end_index][:min_length], label='Mask', color='blue')
-            ax.plot(device_interval, scaled_device_data, label='Device', color='red')
+            ax.plot(mask_interval, mask_edf_data[0, mask_start_index:mask_end_index][:min_length], label="Mask", color="blue")
+            ax.plot(device_interval, scaled_device_data, label="Device", color="red")
 
-            ax.axhline(self.pressure_limit, label='Grenzwert', color='green', linestyle='dashed')
-            ax.legend(loc='upper center', ncol=3)
+            ax.axhline(self.pressure_limit, label="Grenzwert", color="green", linestyle="dashed")
+            ax.legend(loc="upper center", ncol=3)
 
             ###################################################################
             # TEST ATEMZUG MARKIERUNG
@@ -190,6 +190,9 @@ class AtemzugValidierungLogic:
 
             # Setzt die Grenzen der x-Achse entsprechend den Zeitintervallen
             ax.set_xlim(mask_interval[0], device_interval[-1])
+
+            # Aktualisierung des Labels zum Anzeigen der aktuellen Intervalldauer
+            ax.set_xlabel(f"Zeit in Sekunden (aktuelle Intervalldauer: {int(self.interval)}sek)")
 
             # Aktualisiert das Plot Fenster
             plt.draw()
@@ -215,13 +218,13 @@ class AtemzugValidierungLogic:
             self.mask_edf_data = self.raw_mask_edf_data.get_data()
             self.mask_edf_times = self.raw_mask_edf_data.n_times
             # Dauer der Beatmung = Gesamtanzahl der Zeitpunkte / Abtastrate (sfreq)
-            self.duration_mask = self.mask_edf_times / self.mask_edf_meta_data['sfreq']
+            self.duration_mask = self.mask_edf_times / self.mask_edf_meta_data["sfreq"]
 
             # Daten für device.edf Datei
             self.device_edf_meta_data = self.raw_device_edf_data.info
             self.device_edf_data = self.raw_device_edf_data.get_data()
             self.device_edf_times = self.raw_device_edf_data.n_times
-            self.duration_device = self.device_edf_times / self.device_edf_meta_data['sfreq']
+            self.duration_device = self.device_edf_times / self.device_edf_meta_data["sfreq"]
 
             self.plot_edf_data()
 
@@ -232,7 +235,7 @@ class AtemzugValidierungLogic:
     # Funktion um EDF-Datei zu plotten
     def plot_edf_data(self):
         # Schließt alle geöffneten Plots
-        plt.close('all')
+        plt.close("all")
         self.time_difference_start, self.time_difference_end = self.sync_edf_data()
 
         # Erstellt eine Figur in der geplottet wird
@@ -245,9 +248,9 @@ class AtemzugValidierungLogic:
         scaled_device_data = self.scale_factor * self.device_edf_data[0, :]
 
         # Erzeugt Arrays mit zeitpunkten für Mask- und Device-Kurve
-        mask_time = np.arange(0, self.duration_mask, 1 / self.mask_edf_meta_data['sfreq'])
+        mask_time = np.arange(0, self.duration_mask, 1 / self.mask_edf_meta_data["sfreq"])
         device_time = np.arange(0 + self.time_difference_start[0],
-                                self.duration_device + self.time_difference_end[0], 1 / self.device_edf_meta_data['sfreq'])
+                                self.duration_device + self.time_difference_end[0], 1 / self.device_edf_meta_data["sfreq"])
 
         # Bestimmt das kürzere Array aus Mask oder Device
         min_length = min(len(mask_time), len(device_time))
@@ -258,9 +261,9 @@ class AtemzugValidierungLogic:
         scaled_device_data = scaled_device_data[:min_length]
 
         # Plot der Mask-Kurve und der skalierten Device-Kurve
-        ax.plot(mask_time, self.mask_edf_data[0, :min_length], label='Mask', color='blue')
-        ax.plot(device_time, scaled_device_data, label='Device', color='red')
-        ax.legend(loc='upper center', ncol=3)
+        ax.plot(mask_time, self.mask_edf_data[0, :min_length], label="Mask", color="blue")
+        ax.plot(device_time, scaled_device_data, label="Device", color="red")
+        ax.legend(loc="upper center", ncol=3)
 
         # Tkinter-Canvas-Objekt wird erstellt und Figur wird gezeichnet
         canvas = FigureCanvasTkAgg(fig)
@@ -277,19 +280,19 @@ class AtemzugValidierungLogic:
             mat_file = loadmat(mat_file_path)  # öffnet die MAT-Datei mit scipy und speichert die Daten in mat_file
 
             # Abfrage ob Schlüssel "rawDevice" in der MAT-Datei enthalten ist
-            if 'rawDevice' in mat_file:
-                my_data = mat_file['rawDevice']  # extrahiert die Daten unter dem Schlüssel "rawDevice" und speichert diese unter my_data
+            if "rawDevice" in mat_file:
+                my_data = mat_file["rawDevice"]  # extrahiert die Daten unter dem Schlüssel "rawDevice" und speichert diese unter my_data
                 self.plot_mat_data(my_data)
                 print(my_data)  # druckt zum Test die Daten in der Konsole aus (NICHT NOTWENDIG)
 
             # Abfrage ob Schlüssel "rawMask" in der MAT-Datei enthalten ist
-            elif 'rawMask' in mat_file:
-                my_data = mat_file['rawMask']
+            elif "rawMask" in mat_file:
+                my_data = mat_file["rawMask"]
                 self.plot_mat_data(my_data)
                 print(my_data)  # druckt zum Test die Daten in der Konsole aus (NICHT NOTWENDIG)
 
             else:
-                print('Schlüssel "my_data" nicht gefunden in der .mat-Datei.')
+                print("Schlüssel 'my_data' nicht gefunden in der .mat-Datei.")
 
         # es wird eine Fehlermeldung angezeigt, sollte ein Fehler beim Aufrufen einer MAT-Datei auftreten
         except Exception as error_code:
@@ -298,12 +301,12 @@ class AtemzugValidierungLogic:
     def plot_mat_data(self, my_data):
         fig, ax = plt.subplots(1, 1, figsize=(10, 4))
         ax.plot(my_data)
-        ax.set_xlabel('X-Achse')
-        ax.set_ylabel('Y-Achse')
-        ax.set_title('Grafische Darstellung der Daten aus der .mat-Datei')
+        ax.set_xlabel("X-Achse")
+        ax.set_ylabel("Y-Achse")
+        ax.set_title("Grafische Darstellung der Daten aus der .mat-Datei")
         canvas = FigureCanvasTkAgg(fig)
 
-        if hasattr(self, 'canvas') and self.canvas:
+        if hasattr(self, "canvas") and self.canvas:
             self.canvas.get_tk_widget().destroy()
 
         self.canvas = canvas
